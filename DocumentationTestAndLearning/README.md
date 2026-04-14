aed8acd4a30a73adabd5a2f438cfcaa71ab6c52e
jayounghoyos <jayoungh@eafit.edu.co>
2026-04-14 16:34:09 -0500

    Refactor README and enhance MaxAndSkipEnv with detailed logging and observation buffering (Documentation testing )

--- DocumentationTestAndLearning/README.md
+++ DocumentationTestAndLearning/README.md
@@ -1,3 +1,9 @@
-# Don't delete this folder.
+# Don't Delete This Folder
 
-This part of the project help us to test examples and codes that the Gymnasium or ATARI API show us and help us to understand better everything
+This part of the project helps us test examples and code that the Gymnasium or ATARI API provides, to better understand how everything works.
+
+## Helpful Links
+
+- [Gymnasium Wrapper.step](https://gymnasium.farama.org/api/wrappers/#gymnasium.Wrapper.step)
+- [Gymnasium Env.step](https://gymnasium.farama.org/api/env/#gymnasium.Env.step)
+
--- wrappers.py
+++ wrappers.py
@@ -2,19 +2,52 @@ import gymnasium as gymn
 from config import Config
 from gymnasium.wrappers import (RecordEpisodeStatistics, ResizeObservation, GrayscaleObservation, FrameStackObservation,)
 import numpy as np
+import ale_py
 
 class MaxAndSkipEnv(gymn.Wrapper):
     def __init__(self,env, skip=4):
         super().__init__(env)
-        self.skip = skip
+        self._skip = skip
+
         self.__obs_buffer = np.zeros((2,) + env.observation_space.shape, dtype=env.observation_space.dtype)
         
     def step(self,action):
         total_reward = 0.0
-        for step in range(0,self.skip):
-            self.env.step(action)
+
+        for i in range(0,self._skip):
+            obs, reward, terminated,truncated, info = self.env.step(action)
+            total_reward += reward
+            print(f"[MaxAndSkipEnv] frame {i} | reward: {reward} | donde: {terminated or truncated}")
+            
+            # Buffer
+            if i ==  self._skip -2:
+                self.__obs_buffer[0] = obs
+            if i == self._skip -1:
+                self.__obs_buffer[1] = obs
+
+            if terminated or truncated:
+                print(f"[MaxAndSkipEnv] the episode finished in frame {i}")
+                break
+
+        max_frame = self.__obs_buffer.max(axis=0)
+        print(f"[MaxAndSkipEnv] max_frame shape: {max_frame.shape}")
+        return max_frame, total_reward, terminated, truncated, info
+
 
 def make_env(env_id, render_mode=None):
     env = gymn.make(env_id,render_mode=render_mode)
+    env = MaxAndSkipEnv(env,skip=4)
+    env = RecordEpisodeStatistics(env)
+    env = ResizeObservation(env, (84,84))
+    env = GrayscaleObservation(env)
+    env = FrameStackObservation(env, stack_size=4)
 
     return env
+
+
+if __name__ == "__main__":
+    env = make_env("ALE/Breakout-v5", render_mode="human")
+    obs, _ = env.reset()
+    print(obs.shape) # (4,84,84)
+    print(obs.dtype) # uint8
+    env.close()
